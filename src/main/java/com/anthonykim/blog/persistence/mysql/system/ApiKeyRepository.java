490f87b1b427b405490d6408832828b99c0afb00
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-03-27 21:05:54 +0900

    add repository and its service

--- src/main/java/com/anthonykim/blog/persistence/mysql/system/ApiKeyRepository.java
+++ src/main/java/com/anthonykim/blog/persistence/mysql/system/ApiKeyRepository.java
@@ -1,2 +1,11 @@
-package com.anthonykim.blog.persistence.mysql.system;public interface ApiKeyRepository {
+package com.anthonykim.blog.persistence.mysql.system;
+
+import com.anthonykim.blog.entity.system.ApiKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
 }
--- src/main/java/com/anthonykim/blog/service/mysql/system/ApiKeyService.java
+++ src/main/java/com/anthonykim/blog/service/mysql/system/ApiKeyService.java
@@ -1,2 +1,16 @@
-package com.anthonykim.blog.service.mysql.system;public class ApiKeyService {
+package com.anthonykim.blog.service.mysql.system;
+
+import com.anthonykim.blog.persistence.mysql.system.ApiKeyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ApiKeyService {
+    private final EntityManager entityManager;
+    private final ApiKeyRepository apiKeyRepository;
 }
