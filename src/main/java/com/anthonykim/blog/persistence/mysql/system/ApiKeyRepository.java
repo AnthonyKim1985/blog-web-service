291dbba9a010adbe9a0e0251c3e8f7fc90375cee
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-03-27 21:05:36 +0900

    add entity

---
+++ src/main/java/com/anthonykim/blog/persistence/mysql/system/ApiKeyRepository.java
@@ -0,0 +1,2 @@
+package com.anthonykim.blog.persistence.mysql.system;public interface ApiKeyRepository {
+}
---
+++ src/main/java/com/anthonykim/blog/service/mysql/system/ApiKeyService.java
@@ -0,0 +1,2 @@
+package com.anthonykim.blog.service.mysql.system;public class ApiKeyService {
+}
