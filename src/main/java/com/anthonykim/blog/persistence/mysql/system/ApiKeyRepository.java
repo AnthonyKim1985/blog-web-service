bf27f32f8801ef0dadb92a392a1b234a5e8913a6
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-03-27 21:22:18 +0900

    add query method

--- src/main/java/com/anthonykim/blog/persistence/mysql/system/ApiKeyRepository.java
+++ src/main/java/com/anthonykim/blog/persistence/mysql/system/ApiKeyRepository.java
@@ -4,8 +4,10 @@ import com.anthonykim.blog.entity.system.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
+    Optional<ApiKey> findByApiKeyValue(String apiKeyValue);
 }
